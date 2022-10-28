279bda404bacb8a71fbdc6e8a5708edffc237470
SamiraHaji <samirahaji69@gmail.com>
2022-10-28 19:24:42 +0200

    Update Layout

--- components/Layout.js
+++ components/Layout.js
@@ -32,7 +32,7 @@ export default function Layout({ title, children }) {
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
         <footer className="flex h-10 justify-center items-center shadow-inner">
-          Footer
+          <p> Copyright © 2022 Zona</p>
         </footer>
       </div>
     </>
